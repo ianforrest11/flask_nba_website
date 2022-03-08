f8f0dda0f5a86910c11450ab39b161b87b9f22c6
Ian Forrest <iforrest@COLOILS.local>
2022-03-08 13:42:00 -0500

    batch script test

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
+cd C:\Users\iforrest\Desktop\Projects\flask_nba_website
 C:\Users\iforrest\.virtualenvs\flask_nba_website-Sc_vDla7\Scripts\python.exe C:\Users\iforrest\Desktop\Projects\flask_nba_website\update.py
